8576f53e1e0a772af2afe55921f173f589a2d995
ajaybarath <ajaybarath3@gmail.com>
2021-11-08 17:06:47 +0530

    Addded: logging message of the env running

--- src/main/java/com/bridgelabz/springaddressbookapp/SpringAddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/springaddressbookapp/SpringAddressBookAppApplication.java
@@ -3,14 +3,15 @@ package com.bridgelabz.springaddressbookapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class SpringAddressBookAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringAddressBookAppApplication.class, args);
-        log.info("Addressbook application started");
+        ApplicationContext context = SpringApplication.run(SpringAddressBookAppApplication.class, args);
+        log.info("Addressbook application started in {} environment", context.getEnvironment().getProperty("environment"));
     }
 
 }
