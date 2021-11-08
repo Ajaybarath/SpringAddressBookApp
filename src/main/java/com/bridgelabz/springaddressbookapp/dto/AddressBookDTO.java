29d629fbd43dad5457007d8c4e7f8eac6e6e6cac
ajaybarath <ajaybarath3@gmail.com>
2021-11-08 17:16:48 +0530

    Addded: regex patern check to the varibles

--- src/main/java/com/bridgelabz/springaddressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/springaddressbookapp/dto/AddressBookDTO.java
@@ -2,10 +2,15 @@ package com.bridgelabz.springaddressbookapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Pattern;
+
 public @Data
 class AddressBookDTO {
 
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Name is Invalid")
     String name;
+
+    @Pattern(regexp = "^([+]?([0-9]{2})?([6-9]{1}[0-9]{9}))$", message = "Phone number is Invalid")
     String phone;
 
     public AddressBookDTO(String name, String phone) {
